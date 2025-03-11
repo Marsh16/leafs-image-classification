964ddfd705cb0669f21198dbb250f4b7dc321284
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-03-11 18:37:43 +0700

    testing

--- api/index.py
+++ api/index.py
@@ -1,32 +1,30 @@
-from flask import Flask, request, jsonify
-from prediction_model.classifier import predict_image
+from flask import Flask
+# , request, jsonify
+# from prediction_model.classifier import predict_image
 
 # Initialize Flask app
 app = Flask(__name__)
 
-@app.route('/api/hello')
+@app.route('/api/python', methods=['GET'])
 def hello():
     return "hello"
 
-@app.route('/api/predict', methods=['POST'])
-def process_image():
-    """Process the incoming image and return the prediction."""
-    data = request.get_json()
-    if not data or 'data' not in data:
-        return jsonify({'error': 'No image data provided'}), 400
+# @app.route('/api/predict', methods=['POST'])
+# def process_image():
+#     """Process the incoming image and return the prediction."""
+#     data = request.get_json()
+#     if not data or 'data' not in data:
+#         return jsonify({'error': 'No image data provided'}), 400
     
-    file = data.get("data")
-    if not file:
-        return jsonify({"error": "No data provided"}), 400
+#     file = data.get("data")
+#     if not file:
+#         return jsonify({"error": "No data provided"}), 400
 
-    predicted_class, confidence = predict_image(file)
+#     predicted_class, confidence = predict_image(file)
 
-    if predicted_class is None:
-        return jsonify({"error": confidence}), 500
+#     if predicted_class is None:
+#         return jsonify({"error": confidence}), 500
 
-    return jsonify({
-        "result": "This image most likely belongs to {} with a {:.2f}% confidence.".format(predicted_class, confidence)
-    }), 200
-
-if __name__ == '__main__':
-    app.run(debug=True,port=5005)
+#     return jsonify({
+#         "result": "This image most likely belongs to {} with a {:.2f}% confidence.".format(predicted_class, confidence)
+#     }), 200
