d8c565db70244dfe7b42f756068abf35dafcd5bf
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-03-11 17:57:24 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,6 +23,3 @@ def process_image():
     return jsonify({
         "result": "This image most likely belongs to {} with a {:.2f}% confidence.".format(predicted_class, confidence)
     }), 200
-
-if __name__ == '__main__':
-    app.run(debug=True,port=5005)
